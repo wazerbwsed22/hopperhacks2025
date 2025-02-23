262496bc3a0ac05cec61ed648431494fd3f533f4
Aadith2022 <aadith.samala@stonybrook.edu>
2025-02-23 07:42:50 -0500

    added CSS for face recognition

--- src/FaceRecognition.js
+++ src/FaceRecognition.js
@@ -1,6 +1,10 @@
 import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
 import axios from "axios";
+import { FaCamera } from "react-icons/fa6";
+import "./index.css"
+import { HiOutlineLightBulb } from "react-icons/hi";
+
 
 const FaceRecognition = () => {
     const webcamRef = useRef(null);
@@ -23,11 +27,11 @@ const FaceRecognition = () => {
     };
 
     return (
-        <div>
-            <h1>Face Recognition System</h1>
-            <Webcam ref={webcamRef} screenshotFormat="image/jpeg" />
-            <button onClick={capture}>Capture & Recognize</button>
-            <p>Recognized: {recognizedPerson}</p>
+        <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+            <h1 id="webcam-heading">Face Recognition System</h1>
+            <p id="webcam_paragraph"><HiOutlineLightBulb size={17} style={{ marginRight: '8px' }} />Guide: Point camera at someone and take a picture </p>
+            <Webcam ref={webcamRef} screenshotFormat="image/jpeg" style={{border: "2px solid black"}}/>
+            <button onClick={capture} id = "webcam-button" style={{ padding: "10px 20px", width: "640px", fontSize: "16px", cursor: "pointer" }}><FaCamera size = {25}/></button>
         </div>
     );
 };
